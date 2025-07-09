2b1e9b5019049cec20ed2f49335e4ec16b0c8b7a
JavierGonzalezMarti <javigonzalezmarti@gmail.com>
2025-07-09 11:34:12 +0200

    2

---
+++ 02/02/_2App.swift
@@ -0,0 +1,17 @@
+//
+//  _2App.swift
+//  02
+//
+//  Created by Javi on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct _2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 02/02Tests/_2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  _2Tests.swift
+//  02Tests
+//
+//  Created by Javi on 9/7/25.
+//
+
+import Testing
+
+struct _2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
