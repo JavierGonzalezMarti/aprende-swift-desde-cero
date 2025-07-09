34534fcde5d5f53799ef1ae770564b184d4f05ef
JavierGonzalezMarti <javigonzalezmarti@gmail.com>
2025-07-09 12:46:37 +0200

    Ejercicio 01 - variables

---
+++ 01 - Variables/01 - Variables/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01 - Variables
+//
+//  Created by Javi on 9/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
