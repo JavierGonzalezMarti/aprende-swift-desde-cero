f5747790d7506107c5a54795e4f48ed4daba84b9
JavierGonzalezMarti <javigonzalezmarti@gmail.com>
2025-07-09 14:01:50 +0200

    Empezamos con variables

---
+++ 01 Variables/01 Variables/_1_VariablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_VariablesApp.swift
+//  01 Variables
+//
+//  Created by Javi on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct _1_VariablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 01 Variables/01 VariablesTests/_1_VariablesTests.swift
@@ -0,0 +1,16 @@
+//
+//  _1_VariablesTests.swift
+//  01 VariablesTests
+//
+//  Created by Javi on 9/7/25.
+//
+
+import Testing
+
+struct _1_VariablesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
