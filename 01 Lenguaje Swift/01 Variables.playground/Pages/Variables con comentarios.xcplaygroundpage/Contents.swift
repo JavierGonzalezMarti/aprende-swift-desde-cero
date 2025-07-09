071094d6dd1f12c815b8bcd5eaa8a934ed4c3207
JavierGonzalezMarti <javigonzalezmarti@gmail.com>
2025-07-09 16:05:11 +0200

    Trabajo con variables

---
+++ 01 Lenguaje Swift/01 Variables.playground/Pages/Variables con comentarios.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+// Esta línea le dice al programa que traiga un conjunto de herramientas llamadas UIKit que nos permiten usar funciones para crear y manejar elementos gráficos en la aplicación
+
+var saludo = "Hola, vamos a aprender Swift"
+// Aquí estamos creando un espacio de memoria llamado saludo en el que guardamos el texto "Hola, vamos a aprender Swift" para usarlo después
+
+print(saludo)
+// Esta instrucción hace que el programa muestre en la consola (que es la ventana donde vemos los mensajes de texto) el contenido que hay dentro de saludo
+
+saludo = "Hola, ahora he cambiado el valor de la variable saludo"
+// Con esta línea le pedimos al programa que reemplace el texto que había guardado antes en saludo por el nuevo mensaje "Hola, ahora he cambiado el valor de la variable saludo"
+
+print(saludo)
+// Aquí volvemos a mostrar en la consola el nuevo contenido que ahora está dentro de saludo
+
+print("Esto es un mensaje por consola")
+// Esta instrucción muestra directamente en la consola el texto "Esto es un mensaje por consola" sin necesidad de usar una variable intermedia
