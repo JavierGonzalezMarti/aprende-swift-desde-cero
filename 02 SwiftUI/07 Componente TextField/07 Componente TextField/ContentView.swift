fad7741ea5e943d0d2534ba2a2bb1cd17b53f2c6
JavierGonzalezMarti <javigonzalezmarti@gmail.com>
2025-07-17 15:21:00 +0200

    Actualizado

--- 02 SwiftUI/07 Componente TextField/07 Componente TextField/ContentView.swift
+++ 02 SwiftUI/07 Componente TextField/07 Componente TextField/ContentView.swift
@@ -8,13 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textoQueEscribeElUsuario1 = ""
+    @State var textoQueEscribeElUsuario2 = ""
+    @State var textoQueEscribeElUsuario3 = ""
+    @State var textoQueEscribeElUsuario4 = ""
+    @State var textoQueEscribeElUsuario5 = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            VStack (spacing: 25){
+                TextField(
+                    "Teclado por defecto",
+                    text: $textoQueEscribeElUsuario1
+                )
+                .keyboardType(.default)
+                .padding(20)
+                .background(Color.gray.opacity(0.2))
+                .padding(.horizontal, 30)
+             
+
+                TextField(
+                    "Teclado de email",
+                    text: $textoQueEscribeElUsuario2
+                )
+                .keyboardType(.emailAddress)
+                .padding(20)
+                .background(Color.gray.opacity(0.2))
+                .padding(.horizontal, 30)
+                
+
+                TextField(
+                    "Teclado números",
+                    text: $textoQueEscribeElUsuario3
+                )
+                .keyboardType(.numberPad)
+                .padding(20)
+                .background(Color.gray.opacity(0.2))
+                .padding(.horizontal, 30)
+               
+
+                TextField(
+                    "Teclado URL",
+                    text: $textoQueEscribeElUsuario4
+                )
+                .keyboardType(.URL)
+                .padding(20)
+                .background(Color.gray.opacity(0.2))
+                .padding(.horizontal, 30)
+                
+                TextField(
+                    "Teclado Twitter",
+                    text: $textoQueEscribeElUsuario5
+                )
+                .keyboardType(.twitter)
+                .padding(20)
+                .background(Color.gray.opacity(0.2))
+                .padding(.horizontal, 30)
+              
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Lo que escribiste es:")
+
+                    if !textoQueEscribeElUsuario1.isEmpty {
+                        Text(textoQueEscribeElUsuario1)
+                    }
+                    if !textoQueEscribeElUsuario2.isEmpty {
+                        Text(textoQueEscribeElUsuario2)
+                    }
+                    if !textoQueEscribeElUsuario3.isEmpty {
+                        Text(textoQueEscribeElUsuario3)
+                    }
+                    if !textoQueEscribeElUsuario4.isEmpty {
+                        Text(textoQueEscribeElUsuario4)
+                    }
+                    if !textoQueEscribeElUsuario5.isEmpty {
+                        Text(textoQueEscribeElUsuario5)
+                    }
+                }
+                .padding(.top, 20)
+            }
+         
+
+            Spacer()
         }
+        .frame(maxHeight: .infinity,alignment: .top)
         .padding()
     }
 }
