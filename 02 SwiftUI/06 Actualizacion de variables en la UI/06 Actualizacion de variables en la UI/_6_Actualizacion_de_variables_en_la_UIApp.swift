9f5a94a7dbea037852198a1a9d3893e36b6ed0d5
JavierGonzalezMarti <javigonzalezmarti@gmail.com>
2025-07-17 10:31:14 +0200

    creado

---
+++ 02 SwiftUI/06 Actualizacion de variables en la UI/06 Actualizacion de variables en la UI/_6_Actualizacion_de_variables_en_la_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_Actualizacion_de_variables_en_la_UIApp.swift
+//  06 Actualizacion de variables en la UI
+//
+//  Created by Javi on 17/7/25.
+//
+
+import SwiftUI
+
+@main
+struct _6_Actualizacion_de_variables_en_la_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 02 SwiftUI/06 Actualizacion de variables en la UI/06 Actualizacion de variables en la UITests/_6_Actualizacion_de_variables_en_la_UITests.swift
@@ -0,0 +1,16 @@
+//
+//  _6_Actualizacion_de_variables_en_la_UITests.swift
+//  06 Actualizacion de variables en la UITests
+//
+//  Created by Javi on 17/7/25.
+//
+
+import Testing
+
+struct _6_Actualizacion_de_variables_en_la_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
